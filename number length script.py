96c7b4dde20dab13be8d7636322c94ef48652d2c
SISIpalox <157533232+SISIpalox@users.noreply.github.com>
2024-01-23 21:58:13 +0000

    Create number length script.py

---
+++ number length script.py
@@ -0,0 +1,28 @@
+# Example sequence
+sequence = [1, 5, 6, 1, 7, 8, 5, 3, 8, 1, 9, 0, 9, 9, 8, 7, 5, 2, 7, 9, 6, 5, 1, 6, 7, 3, 0, 3, 3, 6, 9, 5, 4, 9, 3, 2, 5, 7, 2, 9, 1, 0, 1, 1, 2, 3, 5, 8, 3, 1, 4, 5, 9, 4, 3, 7, 0, 7, 7, 4]
+
+# Counting the number of elements in the sequence
+number_of_elements = len(sequence)
+
+print("The number of elements in the sequence is:", number_of_elements)
+
+
+
+
+# Example sequence
+sequence = [1, 3, 4, 7, 1, 8, 9, 7, 6, 3, 9, 2]
+
+# Counting the number of elements in the sequence
+number_of_elements = len(sequence)
+
+print("The number of elements in the sequence is:", number_of_elements)
+
+
+
+# Example sequence
+sequence = [0, 2, 2, 4, 6, 0, 6, 6, 2, 8, 0, 8, 8, 6, 4, 0, 4, 4, 8, 2]
+
+# Counting the number of elements in the sequence
+number_of_elements = len(sequence)
+
+print("The number of elements in the sequence is:", number_of_elements)
